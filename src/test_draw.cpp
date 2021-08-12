722f337690041fada40a73b80f429147f5d78e45
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-08-12 23:15:54 +0800

    add simple drawing of robot

---
+++ src/test_draw.cpp
@@ -0,0 +1,303 @@
+// Dear ImGui: standalone example application for GLFW + OpenGL 3, using
+// programmable pipeline (GLFW is a cross-platform general purpose library for
+// handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation,
+// etc.) If you are new to Dear ImGui, read documentation from the docs/ folder
+// + read the top of imgui.cpp. Read online:
+// https://github.com/ocornut/imgui/tree/master/docs
+
+#include <cairo.h>
+#include <math.h>
+
+#include <stdio.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+// About Desktop OpenGL function loaders:
+//  Modern desktop OpenGL doesn't have a standard portable header file to load
+//  OpenGL function pointers. Helper libraries are often used for this purpose!
+//  Here we are supporting a few common ones (gl3w, glew, glad). You may use
+//  another loader/header of your choice (glext, glLoadGen, etc.), or chose to
+//  manually implement your own.
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+#include <GL/gl3w.h>  // Initialize with gl3wInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+#include <GL/glew.h>  // Initialize with glewInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include <glad/glad.h>  // Initialize with gladLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+#include <glad/gl.h>  // Initialize with gladLoadGL(...) or gladLoaderLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+#define GLFW_INCLUDE_NONE  // GLFW including OpenGL headers causes ambiguity or
+                           // multiple definition errors.
+#include <glbinding/Binding.h>  // Initialize with glbinding::Binding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+#define GLFW_INCLUDE_NONE  // GLFW including OpenGL headers causes ambiguity or
+                           // multiple definition errors.
+#include <glbinding/gl/gl.h>
+#include <glbinding/glbinding.h>  // Initialize with glbinding::initialize()
+using namespace gl;
+#else
+#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#endif
+
+// Include glfw3.h after our OpenGL definitions
+#include <GLFW/glfw3.h>
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to
+// maximize ease of testing and compatibility with old VS compilers. To link
+// with VS2010-era libraries, VS2015+ requires linking with
+// legacy_stdio_definitions.lib, which we do using this pragma. Your own project
+// should not be affected, as you are likely to link with a newer binary of GLFW
+// that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && \
+    !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+static void glfw_error_callback(int error, const char* description) {
+  fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+int main(int, char**) {
+  // Setup window
+  glfwSetErrorCallback(glfw_error_callback);
+  if (!glfwInit()) return 1;
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+  // GL ES 2.0 + GLSL 100
+  const char* glsl_version = "#version 100";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+  // GL 3.2 + GLSL 150
+  const char* glsl_version = "#version 150";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // Required on Mac
+#else
+  // GL 3.0 + GLSL 130
+  const char* glsl_version = "#version 130";
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+
+  // only glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // 3.0+ only
+#endif
+
+  // Create window with graphics context
+  GLFWwindow* window =
+      glfwCreateWindow(1280, 720, "Robot kinematics simulator", NULL, NULL);
+  if (window == NULL) return 1;
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);  // Enable vsync
+
+  // Initialize OpenGL loader
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+  bool err = gl3wInit() != 0;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+  bool err = glewInit() != GLEW_OK;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+  bool err = gladLoadGL() == 0;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+  bool err = gladLoadGL(glfwGetProcAddress) ==
+             0;  // glad2 recommend using the windowing library loader instead
+                 // of the (optionally) bundled one.
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+  bool err = false;
+  glbinding::Binding::initialize();
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+  bool err = false;
+  glbinding::initialize([](const char* name) {
+    return (glbinding::ProcAddress)glfwGetProcAddress(name);
+  });
+#else
+  bool err = false;  // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader
+                     // is likely to requires some form of initialization.
+#endif
+  if (err) {
+    fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+    return 1;
+  }
+
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  (void)io;
+  // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable
+  // Keyboard Controls io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; //
+  // Enable Gamepad Controls
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  // ImGui::StyleColorsClassic();
+
+  // Setup Platform/Renderer backends
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init(glsl_version);
+
+  // Our sta
+  ImVec4 clear_color = ImVec4(0.15f, 0.15f, 0.10f, 1.00f);
+
+  GLuint image_texture_;
+
+  glGenTextures(1, &image_texture_);
+  glBindTexture(GL_TEXTURE_2D, image_texture_);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // the following two are required on WebGL for non power-of-two textures
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  ImVec2 position;
+
+  position.x = 0.0;
+  position.y = 0.0;
+
+  // Main loop
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::SetNextWindowPos(position);
+    ImGui::SetNextWindowSize(io.DisplaySize);
+
+    ImGui::Begin("Cairo Canvas", NULL,
+                 ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar |
+                     ImGuiWindowFlags_NoBringToFrontOnFocus |
+                     ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoCollapse |
+                     ImGuiWindowFlags_NoScrollbar |
+                     ImGuiWindowFlags_NoBackground);
+
+    for (int i = 0; i < IM_ARRAYSIZE(io.KeysDown); i++) {
+      if (ImGui::IsKeyDown(i)) {
+        if (i == 0x106) ImGui::Text("Right key pressed!\n");
+        if (i == 0x107) ImGui::Text("Left key pressed!\n");
+        if (i == 0x108) ImGui::Text("Down key pressed!\n");
+        if (i == 0x109) ImGui::Text("Up key pressed!\n");
+      }
+    }
+
+    cairo_surface_t* cairo_surface = nullptr;
+    cairo_t* cr = nullptr;
+    cairo_surface =
+        cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 300.0, 300.0);
+    cr = cairo_create(cairo_surface);
+
+    double xc = 128.0;
+    double yc = 128.0;
+    double radius = 100.0;
+    double angle1 = 45.0 * (M_PI / 180.0);  /* angles are specified */
+    double angle2 = 180.0 * (M_PI / 180.0); /* in radians           */
+
+    float cx, cy;
+    float front_wheel_width = 10.0;
+    float front_wheel_height = 30.0;
+    float rear_wheel_width = 12.5;
+    float rear_wheel_height = 30.0;
+
+    float front_wheel_left_pos = 100.0;
+
+    float wheel_base = 100.0;
+    float track_width = 40.0;
+    cx = 100.0;
+    cy = 100.0;
+
+    // front left wheel
+    cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
+    cairo_set_line_width(cr, 2.0);
+    cairo_rectangle(cr, cx, cy, front_wheel_width, front_wheel_height);
+    cairo_stroke(cr);
+
+    // front horizontal line
+    cx += front_wheel_width;
+    cy += front_wheel_height / 2.0;
+    cairo_move_to(cr, cx, cy);
+    cx += track_width;
+    cairo_line_to(cr, cx, cy);
+    cairo_stroke(cr);
+
+    // front right wheel
+    cy -= front_wheel_height / 2.0;
+    cairo_rectangle(cr, cx, cy, front_wheel_width, front_wheel_height);
+    cairo_stroke(cr);
+
+    // wheel base
+    cy += front_wheel_height / 2.0;
+    cx -= track_width / 2.0;
+    cairo_move_to(cr, cx, cy);
+    cy += wheel_base; 
+    cairo_line_to(cr, cx, cy);
+    cairo_stroke(cr);
+
+    // rear left wheel
+    cy -= rear_wheel_height / 2.0;
+    cx -= track_width / 2.0;
+    cairo_rectangle(cr, cx - rear_wheel_width, cy, rear_wheel_width, rear_wheel_height);
+    cairo_stroke(cr);
+
+    // rear horizontal line
+    cy += rear_wheel_height / 2.0;
+    cairo_move_to(cr, cx, cy);
+    cx += track_width;
+    cairo_line_to(cr, cx, cy);
+    cairo_stroke(cr);
+
+    // rear right wheel
+    cy -= rear_wheel_height / 2.0;
+    cairo_rectangle(cr, cx, cy, rear_wheel_width, rear_wheel_height);
+    cairo_stroke(cr);
+
+    glBindTexture(GL_TEXTURE_2D, image_texture_);
+
+    // convert surface to OpenGL texture
+    unsigned char* data = cairo_image_surface_get_data(cairo_surface);
+    glTexImage2D(GL_TEXTURE_2D, 0, 4, 300, 300, 0, GL_BGRA, GL_UNSIGNED_BYTE,
+                 data);
+
+    // unbind texture
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    ImGui::Image((void*)(intptr_t)image_texture_,
+                 ImGui::GetContentRegionAvail(), ImVec2(0, 0), ImVec2(1, 1),
+                 ImVec4(1, 1, 1, 1), ImVec4(0, 0, 0, 0.0));
+
+    cairo_surface_destroy(cairo_surface);
+    cairo_destroy(cr);
+
+    ImGui::End();
+    // Rendering
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w,
+                 clear_color.z * clear_color.w, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
+  }
+
+  // Cleanup
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
