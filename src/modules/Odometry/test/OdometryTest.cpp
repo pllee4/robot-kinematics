b1b1e36601daeab01e3d3a74a3fac0722fd01f7e
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-09-03 08:01:10 +0800

    src: modules: Odometry: update test with quaternion

--- src/modules/Odometry/test/OdometryTest.cpp
+++ src/modules/Odometry/test/OdometryTest.cpp
@@ -18,25 +18,50 @@ TEST(Odometry, Initialization) {
   EXPECT_EQ(odometry_instance.odom.x, 0.0);
   EXPECT_EQ(odometry_instance.odom.y, 0.0);
   EXPECT_EQ(odometry_instance.odom.theta, 0.0);
+  EXPECT_EQ(odometry_instance.odom.quaternion.w, 0.0);
+  EXPECT_EQ(odometry_instance.odom.quaternion.x, 0.0);
+  EXPECT_EQ(odometry_instance.odom.quaternion.y, 0.0);
+  EXPECT_EQ(odometry_instance.odom.quaternion.z, 0.0);
 }
 
 TEST(Odometry, ResetOdometry) {
   odometry_instance.odom.x = 1.0;
   odometry_instance.odom.y = 2.0;
   odometry_instance.odom.theta = 3.0;
+
+  odometry_instance.odom.quaternion.w = 0.1;
+  odometry_instance.odom.quaternion.x = 0.2;
+  odometry_instance.odom.quaternion.y = 0.3;
+  odometry_instance.odom.quaternion.z = 0.4;
+
   ResetOdometry(&odometry_instance);
+
   EXPECT_FLOAT_EQ(odometry_instance.odom.x, 0.0);
   EXPECT_FLOAT_EQ(odometry_instance.odom.y, 0.0);
   EXPECT_FLOAT_EQ(odometry_instance.odom.theta, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.w, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.x, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.y, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.z, 0.0);
 }
 
 TEST(Odometry, GetOdometry) {
   odometry_instance.odom.x = 1.0;
   odometry_instance.odom.y = 2.0;
   odometry_instance.odom.theta = 3.0;
+
+  odometry_instance.odom.quaternion.w = 0.1;
+  odometry_instance.odom.quaternion.x = 0.2;
+  odometry_instance.odom.quaternion.y = 0.3;
+  odometry_instance.odom.quaternion.z = 0.4;
+
   EXPECT_FLOAT_EQ(GetOdometry(&odometry_instance).x, 1.0);
   EXPECT_FLOAT_EQ(GetOdometry(&odometry_instance).y, 2.0);
   EXPECT_FLOAT_EQ(GetOdometry(&odometry_instance).theta, 3.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.w, 0.1);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.x, 0.2);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.y, 0.3);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.z, 0.4);
 }
 
 TEST(Odometry, OdometryLoop) {
@@ -50,11 +75,19 @@ TEST(Odometry, OdometryLoop) {
   EXPECT_FLOAT_EQ(odometry_instance.odom.x, 1.0);
   EXPECT_FLOAT_EQ(odometry_instance.odom.y, 0.0);
   EXPECT_FLOAT_EQ(odometry_instance.odom.theta, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.w, 1.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.x, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.y, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.z, 0.0);
 
   OdometryLoop(&odometry_instance, odometry_input);
   EXPECT_FLOAT_EQ(odometry_instance.odom.x, 2.0);
   EXPECT_FLOAT_EQ(odometry_instance.odom.y, 0.0);
   EXPECT_FLOAT_EQ(odometry_instance.odom.theta, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.w, 1.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.x, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.y, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.z, 0.0);
 
   odometry_input.linear_vel = 1.0;
   odometry_input.angular_vel = 1.0;
@@ -64,9 +97,32 @@ TEST(Odometry, OdometryLoop) {
   EXPECT_FLOAT_EQ(odometry_instance.odom.x, 3.0);
   EXPECT_FLOAT_EQ(odometry_instance.odom.y, 0.0);
   EXPECT_FLOAT_EQ(odometry_instance.odom.theta, 1.0);
+  EXPECT_NEAR(odometry_instance.odom.quaternion.w, 0.877583, 1.0e-6);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.x, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.y, 0.0);
+  EXPECT_NEAR(odometry_instance.odom.quaternion.z, 0.479426, 1.0e-6);
 
   OdometryLoop(&odometry_instance, odometry_input);
   EXPECT_FLOAT_EQ(odometry_instance.odom.x, 3.0 + cos(1.0));
   EXPECT_FLOAT_EQ(odometry_instance.odom.y, 0.0 + sin(1.0));
   EXPECT_FLOAT_EQ(odometry_instance.odom.theta, 2.0);
+
+  EXPECT_NEAR(odometry_instance.odom.quaternion.w, 0.540302, 1.0e-6);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.x, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.y, 0.0);
+  EXPECT_NEAR(odometry_instance.odom.quaternion.z, 0.841471, 1.0e-6);
+
+  odometry_input.linear_vel = -1.0;
+  odometry_input.angular_vel = -3.0;
+  odometry_input.dt = 1.0;
+
+  OdometryLoop(&odometry_instance, odometry_input);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.x, 3.0 + cos(1.0) - cos(2.0));
+  EXPECT_FLOAT_EQ(odometry_instance.odom.y, 0.0 + sin(1.0) - sin(2.0));
+  EXPECT_FLOAT_EQ(odometry_instance.odom.theta, -1.0);
+
+  EXPECT_NEAR(odometry_instance.odom.quaternion.w, 0.877583, 1.0e-6);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.x, 0.0);
+  EXPECT_FLOAT_EQ(odometry_instance.odom.quaternion.y, 0.0);
+  EXPECT_NEAR(odometry_instance.odom.quaternion.z, -0.479426, 1.0e-6);
 }
