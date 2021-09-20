e91e3086643064791d2205cf6c88082db89f106c
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-09-20 23:30:07 +0800

    src: ui: add window

---
+++ src/ui/window/include/window/window.hpp
@@ -0,0 +1,38 @@
+/*
+ * window.hpp
+ * Created on: Sep 20, 2021 22:28
+ * Description:
+ *
+ * Copyright (c) 2021 Pin Loon Lee (pllee4)
+ */
+
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+
+#include <string>
+
+#include <GLFW/glfw3.h>
+
+namespace pllee4 {
+
+class Window {
+ public:
+  Window(uint16_t width = 640, uint16_t height = 480,
+         std::string title = "GUI Window");
+  ~Window();
+
+  bool ShouldClose();
+  void StartFrame();
+  void RenderFrame();
+
+ private:
+  static constexpr const char* glsl_version_ = "#version 130";
+  static constexpr int glfw_context_version_major_ = 3;
+  static constexpr int glfw_context_version_minor_ = 0;
+
+  GLFWwindow* glfw_window_;
+
+  ImVec4 clear_color_ = ImVec4(0.0f, 0.0f, 0.0f, 0.10f);
+};
+}  // namespace pllee4
+#endif /* WINDOW_HPP */
