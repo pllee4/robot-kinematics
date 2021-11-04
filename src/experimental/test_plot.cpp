3bf0df8ebccc4fb4ef5e4dce1a6fef17c7a0697f
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-11-04 14:40:31 +0800

    srcL experimental: test_plot: plot few function graph

--- src/experimental/test_plot.cpp
+++ src/experimental/test_plot.cpp
@@ -25,12 +25,15 @@
 #include "window/window.hpp"
 
 #include <iostream>
+#include <vector>
 
 using namespace pllee4;
 
 int main(int, char**) {
   // Setup window
-  Window window(1080, 720, "Trajectory Planning");
+  auto constexpr window_width = 1080;
+  auto constexpr window_height = 960;
+  Window window(window_width, window_height, "Trajectory Response");
 
   ImGuiIO& io = ImGui::GetIO();
   (void)io;
@@ -54,6 +57,29 @@ int main(int, char**) {
   position.x = 0.0;
   position.y = 0.0;
 
+  std::vector<float> x1_data, y1_data;
+  std::vector<float> x2_data, y2_data;
+  std::vector<float> x3_data, y3_data;
+
+  x1_data.reserve(100);
+  x2_data.reserve(100);
+  x3_data.reserve(100);
+  y1_data.reserve(100);
+  y2_data.reserve(100);
+  y3_data.reserve(100);
+
+  for (uint32_t i = 1; i <= 100; ++i) {
+    x1_data.push_back(i * 0.1);
+    x2_data.push_back(i * 0.1);
+    x3_data.push_back(i * 0.1);
+  }
+
+  for (uint32_t i = 0; i < 100; ++i) {
+    y1_data.push_back(1 / x1_data[i]);          // 1 / x
+    y2_data.push_back(1 / pow(x2_data[i], 2));  // 1 x^2
+    y3_data.push_back(1 / exp(x3_data[i]));     // 1 / e^x
+  }
+
   // Main loop
   while (!window.ShouldClose()) {
     glfwPollEvents();
@@ -70,14 +96,63 @@ int main(int, char**) {
                      ImGuiWindowFlags_NoScrollbar |
                      ImGuiWindowFlags_NoBackground);
 
-    float x_data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    float y_data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    ImVec2 plot_size{window_width / 2.1, window_height / 3.5};
 
     ImPlot::CreateContext();
-    if (ImPlot::BeginPlot("My Plot")) {
-      ImPlot::PlotLine("My Line Plot", x_data, y_data, 10);
+    if (ImPlot::BeginPlot("y = 1 / x", plot_size)) {
+      ImPlot::SetupAxesLimits(0, 10, 0, 100);
+      for (uint16_t i = 0; i < 100; ++i) {
+        y1_data[i] = (1 / x1_data[i]);
+      }
+      ImPlot::PlotLine("##NoTitle1", &x1_data[0], &y1_data[0], 100);
       ImPlot::EndPlot();
     }
+    ImGui::SameLine();
+    if (ImPlot::BeginPlot("y = 5 / x", plot_size)) {
+      ImPlot::SetupAxesLimits(0, 10, 0, 100);
+      for (uint16_t i = 0; i < 100; ++i) {
+        y1_data[i] = (5 / x1_data[i]);
+      }
+      ImPlot::PlotLine("##NoTitle1", &x1_data[0], &y1_data[0], 100);
+      ImPlot::EndPlot();
+    }
+
+    if (ImPlot::BeginPlot("y = 1 / (x ^ 2)", plot_size)) {
+      ImPlot::SetupAxesLimits(0, 10, 0, 100);
+      for (uint16_t i = 0; i < 100; ++i) {
+        y2_data[i] = (1 / pow(x2_data[i], 2));  // 1 x^2
+      }
+      ImPlot::PlotLine("##NoTitle2", &x2_data[0], &y2_data[0], 100);
+      ImPlot::EndPlot();
+    }
+    ImGui::SameLine();
+    if (ImPlot::BeginPlot("y = 1 / (x ^ 4)", plot_size)) {
+      ImPlot::SetupAxesLimits(0, 10, 0, 100);
+      for (uint16_t i = 0; i < 100; ++i) {
+        y2_data[i] = (1 / pow(x2_data[i], 4));  // 1 x^2
+      }
+      ImPlot::PlotLine("##NoTitle2", &x2_data[0], &y2_data[0], 100);
+      ImPlot::EndPlot();
+    }
+
+    if (ImPlot::BeginPlot("y = 1 / exp(x)", plot_size)) {
+      ImPlot::SetupAxesLimits(0, 10, 0, 100);
+      for (uint16_t i = 0; i < 100; ++i) {
+        y3_data[i] = (1 / exp(x3_data[i]));
+      }
+      ImPlot::PlotLine("##NoTitle3", &x3_data[0], &y3_data[0], 100);
+      ImPlot::EndPlot();
+    }
+    ImGui::SameLine();
+    if (ImPlot::BeginPlot("y = exp(1 / x)", plot_size)) {
+      ImPlot::SetupAxesLimits(0, 10, 0, 100);
+      for (uint16_t i = 0; i < 100; ++i) {
+        y3_data[i] = (exp(1 / x3_data[i]));
+      }
+      ImPlot::PlotLine("##NoTitle3", &x3_data[0], &y3_data[0], 100);
+      ImPlot::EndPlot();
+    }
+
     ImPlot::DestroyContext();
 
     ImGui::End();
